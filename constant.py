b759b5f83b5fea306e0b274648cef9ef480804a1
Kuy CODING <46470594+kuycoding@users.noreply.github.com>
2020-10-15 10:17:50 +0700

    Update constant.py

--- constant.py
+++ constant.py
@@ -1,5 +1,5 @@
-consumer_key = "x9zuaTU1HbDErb9amFHGyY2kY"
-consumer_secret = "6tZlXskc8QD9f8j9FthqZzLFHN6E2aH1nINfMlqJu2pWg3MsEc"
-access_token = "1278541122512408576-ClQSTF1hM173Maz8Sy6n109zJxiTS4"
-access_token_secret = "JqDkBY5iamljZPcrs0ayv0I26UoKa3TEnxZMGZT7EhXoz"
+consumer_key = "xA9zuaTU1HbDErb9amFHGyY2kY"
+consumer_secret = "6AtZlXskc8QD9f8j9FthqZzLFHN6E2aH1nINfMlqJu2pWg3MsEc"
+access_token = "1A278541122512408576-ClQSTF1hM173Maz8Sy6n109zJxiTS4"
+access_token_secret = "JAqDkBY5iamljZPcrs0ayv0I26UoKa3TEnxZMGZT7EhXoz"
 # Apiku jangan dishare ya
